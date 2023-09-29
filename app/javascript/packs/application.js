b5182b9621185fc55ae63d3ce35aa4cf02138d51
arakinoko <strawberry.candy16@gmail.com>
2023-09-29 20:05:44 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "bootstrap"
-import "../stylesheets/application"
 
 
 
