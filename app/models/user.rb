7873a3a34b6dde4564c0726a82b75e92718c569c
arakinoko <strawberry.candy16@gmail.com>
2023-10-01 14:34:58 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :nickname
+  validates :last_name,
+             format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.', allow_blank: true }
+  validates :first_name,
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.', allow_blank: true }
+  validates :last_name_kana,
+            format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.', allow_blank: true }
+  validates :first_name_kana,
+            format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.', allow_blank: true }
+  validates :birth_date
+  end
 end
