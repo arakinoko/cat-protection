c69cb994c86aa51902a7f98d9244147a4dee4b80
arakinoko <strawberry.candy16@gmail.com>
2023-10-12 20:09:44 +0900

    保護猫削除機能

--- app/controllers/protections_controller.rb
+++ app/controllers/protections_controller.rb
@@ -1,4 +1,5 @@
 class ProtectionsController < ApplicationController
+  before_action :set_protection, only: [:edit, :show, :destroy]
 
   def index
     @protections = Protection.order("created_at DESC")
@@ -18,7 +19,6 @@ class ProtectionsController < ApplicationController
   end
 
   def edit
-    @protection = Protection.find(params[:id])
     if @protection.user_id == current_user.id
     else
       redirect_to root_path
@@ -28,19 +28,28 @@ class ProtectionsController < ApplicationController
   def update
     protection = Protection.find(params[:id])
     if protection.update(protection_params)
-      redirect_to root_path
+      redirect_to protection_path(protection)
     else
       render :edit
     end
   end
 
   def show
-    @protection = Protection.find(params[:id])
+  end
+
+  def destroy
+    protection = Protection.find(params[:id])
+    protection.destroy if @protection.user == current_user
+    redirect_to root_path
   end
 
 
-
   private
+
+  def set_protection
+    @protection = Protection.find(params[:id])
+  end
+
   def protection_params
     params.require(:protection).permit(:title, :age, :gender_id, :cat_species_id, :character, :image).merge(user_id: current_user.id)
   end
