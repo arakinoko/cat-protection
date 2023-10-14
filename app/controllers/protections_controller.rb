2130fc6a347bc8ab087d0ba12652d52f2176d405
arakinoko <strawberry.candy16@gmail.com>
2023-10-15 08:30:54 +0900

    ログイン、新規登録のヘッダー追加

--- app/controllers/protections_controller.rb
+++ app/controllers/protections_controller.rb
@@ -1,4 +1,5 @@
 class ProtectionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_protection, only: [:edit, :show, :destroy]
 
   def index
